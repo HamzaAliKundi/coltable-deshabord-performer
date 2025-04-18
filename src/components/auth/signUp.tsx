d576997b7f88ce0d2789fcc4db88e5020d5a4b55
“HamzaAliKundi” <hamza.alee83@gmail.com>
2025-04-18 22:41:45 +0500

    add logout and set user types

--- src/components/auth/signUp.tsx
+++ src/components/auth/signUp.tsx
@@ -135,8 +135,8 @@ const Signup = () => {
                     {...register("userType", { required: true })}
                   >
                     <option value="performer">Performer</option>
-                    <option value="listener">Listener</option>
-                    <option value="producer">Producer</option>
+                    <option value="venues">Venues</option>
+                    <option value="users">Users</option>
                   </select>
                   <div className="absolute right-3 top-3.5 pointer-events-none">
                     <svg
