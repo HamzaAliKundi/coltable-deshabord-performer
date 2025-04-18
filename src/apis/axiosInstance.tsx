1a5f605c00065e21bf9d80ff4678cb199e767956
“HamzaAliKundi” <hamza.alee83@gmail.com>
2025-04-18 16:34:14 +0500

    integrate auth api

--- src/apis/axiosInstance.tsx
+++ src/apis/axiosInstance.tsx
@@ -1,20 +1,18 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://task-management-be-h90g.onrender.com/api/v1",
+  baseURL: "https://coltable-be.onrender.com/api/v1",
   timeout: 10000,
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    if (config.url === "/auth") return config;
+    if (config.url?.startsWith("/auth")) return config;
     const token = localStorage.getItem("token");
     if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
