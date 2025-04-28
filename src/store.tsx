9546dc9f080ac48a62a8de8f488ccbc766448c69
HamzaAliKundi <hamza.alee83@gmail.com>
2025-04-28 21:20:56 +0500

    add review section

--- src/store.tsx
+++ src/store.tsx
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from './apis/auth';
 import { eventsApi } from './apis/event';
 import { profileApi } from './apis/profile';
+import { reviewsApi } from './apis/reviews';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [eventsApi.reducerPath]: eventsApi.reducer,
     [profileApi.reducerPath]: profileApi.reducer,
+    [reviewsApi.reducerPath]: reviewsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, eventsApi.middleware, profileApi.middleware,),
+    getDefaultMiddleware().concat(authApi.middleware, eventsApi.middleware, profileApi.middleware, reviewsApi.middleware, ),
 });
