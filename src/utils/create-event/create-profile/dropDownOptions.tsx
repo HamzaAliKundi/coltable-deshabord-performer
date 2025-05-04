f53ab7a3b0f650dbaa98b96c4f1c59444d5212be
ahsan798 <itsmeahsan98@gmail.com>
2025-05-04 20:05:03 +0500

    changes

---
+++ src/utils/create-event/create-profile/dropDownOptions.tsx
@@ -0,0 +1,6 @@
+export const pronounOptions = [
+  { value: "she/her", label: "She/Her" },
+  { value: "he/him", label: "He/Him" },
+  { value: "they/them", label: "They/Them" },
+  { value: "other", label: "Other" },
+];
